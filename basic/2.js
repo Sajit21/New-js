3180930b42dcbe10d405f42e6cb91d0337c8f290
Sajit21 <sajitmhr13@gmail.com>
2024-08-23 22:07:30 +0545

    jscript

---
+++ basic/2.js
@@ -0,0 +1,28 @@
+let q="hello Media"
+console.log(q)
+
+//string concatination
+let r="hello"
+let t="ram"
+console.log(r +' '+t)
+
+//accesing an postion of an array
+let a=["ram","hari","pari"]
+console.log(a[0])
+
+//for loop section
+for(i=0;i<5;i++){
+    console.log('in loop',i)
+}
+
+//loop in variable
+let o=["ram","hari","pari"]
+for(i=0;i<=a.length;i++)
+{
+    console.log(o[i])
+}
+console.log("end of the loop")
+
+
+//
+ 
