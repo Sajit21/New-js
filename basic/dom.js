1d9816fabd25f409ee0da3cddd498d288893d022
Sajit Maharjan <126838978+Sajit21@users.noreply.github.com>
2024-10-25 11:46:45 +0545

    Update dom.js

--- basic/dom.js
+++ basic/dom.js
@@ -4,13 +4,23 @@ document.getElementById("one").innerHTML="ramlal";
 let hari=document.getElementById("one").innerHTML;
 console.log(hari)
 
-//3div with common class name box //use of forof loop to access all the box classes
+//3div with common class name box
 let hell=document.getElementsByClassName("box");
 let idx=1;
 for( let div of hell){
 div.innerText=div.innerText + ` new content added to ${idx}`;
 idx++;
 }
+
+
+//button create garera add garni 
+//paila button create
+let btn=document.createElement("button")
+btn.innerText="click haan";
+console.log(btn)
+
+let but=document.getElementsByClassName("oho");
+but[0].append(btn);
 
 
 
